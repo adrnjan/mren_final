ac2defdb2c7b5547ca65acd1a7b5b3a752239d01
adrian <113150668+adrnjan@users.noreply.github.com>
2026-03-31 22:50:52 -0400

    Add files via upload

--- fsm.c
+++ fsm.c
@@ -8,21 +8,21 @@ static bool has_active_request = false;
 
 static bool en_route_stop = false;
 
-void fsm_init(Elevator *elevator) {
+void fsm_init(Elevator *elevator) { //initialize with default values
     elevator->current_floor = 1;
     elevator->current_state = STATE_IDLE;
     elevator->doors_open = true;
     has_active_request = false;
     en_route_stop = false;
-    printf("[FSM] Elevator initialized at floor 1. Status: IDLE, Doors: OPEN\n");
+    printf("[FSM] Elevator initialized at floor 1. Status: Idle, Doors: Open\n");
 }
 
 void fsm_step(Elevator *elevator, int current_tick, int *total_wait, int *passengers_served){
     switch (elevator->current_state) {
 
         case STATE_IDLE:
-            if (!has_active_request) {
-                if (!pq_is_empty()) {
+            if (!has_active_request) { //if no active request check if there are any requests in the priority queue
+                if (!pq_is_empty()) { //if there are requests, refresh priorities, extract the highest priority request, set has_active_request to true, and print the extracted request
                     pq_refresh_priorities(elevator->current_floor, elevator->current_state, current_tick);
                     pq_extract(&active_request);
                     has_active_request = true;
@@ -32,22 +32,22 @@ void fsm_step(Elevator *elevator, int current_tick, int *total_wait, int *passen
             }
 
             if (has_active_request) {
-                if (elevator->doors_open) {
+                if (elevator->doors_open) { //"close" the doors (no fsm for closed doors, just a bool)
                     printf("[FSM] Closing doors...\n");
                     elevator->doors_open = false;
                 }
 
-                if (active_request.target_floor > elevator->current_floor) {
+                if (active_request.target_floor > elevator->current_floor) { //if target floor is above current floor, move up
                     elevator->current_state = STATE_MOVING_UP;
-                    printf("[FSM] Transition -> MOVING UP\n");
+                    printf("[FSM] Transition -> Moving Up\n");
                 } 
-                else if (active_request.target_floor < elevator->current_floor) {
+                else if (active_request.target_floor < elevator->current_floor) { //if target floor is below current floor, move down
                     elevator->current_state = STATE_MOVING_DOWN;
-                    printf("[FSM] Transition -> MOVING DOWN\n");
+                    printf("[FSM] Transition -> Moving Down\n");
                 } 
-                else {
+                else { //if target floor is same as current floor, open doors
                     elevator->current_state = STATE_DOORS_OPEN;
-                    printf("[FSM] Transition -> DOORS OPEN (Already on target floor)\n");
+                    printf("[FSM] Transition -> Doors Open (Already on target floor)\n");
                 }
             }
             break;
@@ -59,17 +59,17 @@ void fsm_step(Elevator *elevator, int current_tick, int *total_wait, int *passen
             int wait_time_up = 0;
             if (elevator->current_floor != active_request.target_floor &&
                 pq_size() < MAX_QUEUE_SIZE &&
-                pq_check_and_remove_target(elevator->current_floor, current_tick, &wait_time_up)) {
+                pq_check_and_remove_target(elevator->current_floor, current_tick, &wait_time_up)) { //enroute case(not @detination), wont stop unless there is guarantee the re insertion will succeed, so we check if room in queue.
                 printf("[FSM] En-route stop at floor %d (continuing to floor %d). Passenger wait time: %d ticks\n",
-                       elevator->current_floor, active_request.target_floor, wait_time_up);
-                *total_wait += wait_time_up;
-                (*passengers_served)++;
-                pq_insert(active_request.target_floor, elevator->current_floor, elevator->current_state, active_request.request_tick);
-                en_route_stop = true;
-                elevator->current_state = STATE_DOORS_OPEN;
+                       elevator->current_floor, active_request.target_floor, wait_time_up); //we passed wait time up to calc how long requested existed, so we now print that
+                *total_wait += wait_time_up; //add to total for average calc after
+                (*passengers_served)++; 
+                pq_insert(active_request.target_floor, elevator->current_floor, elevator->current_state, active_request.request_tick); //reinsert original target request to queue(might be at top, not necessarily, depends on priority and maybe new request was made)
+                en_route_stop = true; //set en_route_stop to true for preventing counting passenger twice(once during enroute and again in doors open)
+                elevator->current_state = STATE_DOORS_OPEN; //for next call
                 break;
             }
-            if (elevator->current_floor == active_request.target_floor) {
+            if (elevator->current_floor == active_request.target_floor) { //if we are at target, then simply change to state doors open for next call
                 printf("[FSM] Arrived at target floor %d!\n", elevator->current_floor);
                 elevator->current_state = STATE_DOORS_OPEN;
                 printf("[FSM] Transition -> DOORS OPEN\n");
@@ -85,15 +85,15 @@ void fsm_step(Elevator *elevator, int current_tick, int *total_wait, int *passen
                 pq_size() < MAX_QUEUE_SIZE &&
                 pq_check_and_remove_target(elevator->current_floor, current_tick, &wait_time_down)) {
                 printf("[FSM] En-route stop at floor %d (continuing to floor %d). Passenger wait time: %d ticks\n",
-                       elevator->current_floor, active_request.target_floor, wait_time_down);
+                       elevator->current_floor, active_request.target_floor, wait_time_down); //enroute case, same logic as up
                 *total_wait += wait_time_down;
                 (*passengers_served)++;
-                pq_insert(active_request.target_floor, elevator->current_floor, elevator->current_state, active_request.request_tick);
-                en_route_stop = true;
-                elevator->current_state = STATE_DOORS_OPEN;
+                pq_insert(active_request.target_floor, elevator->current_floor, elevator->current_state, active_request.request_tick); //reinsert original target request to queue(might be at top, not necessarily, depends on priority and maybe new request was made)
+                en_route_stop = true; //set en_route_stop to true for preventing counting passenger twice(once during enroute and again in doors open)
+                elevator->current_state = STATE_DOORS_OPEN; //for next call
                 break;
             }
-            if (elevator->current_floor == active_request.target_floor) {
+            if (elevator->current_floor == active_request.target_floor) { //if we are at target, then simply change to state doors open for next call
                 printf("[FSM] Arrived at target floor %d!\n", elevator->current_floor);
                 elevator->current_state = STATE_DOORS_OPEN;
                 printf("[FSM] Transition -> DOORS OPEN\n");
@@ -102,19 +102,19 @@ void fsm_step(Elevator *elevator, int current_tick, int *total_wait, int *passen
 
         case STATE_DOORS_OPEN:
         {
-            if (en_route_stop) {
+            if (en_route_stop) { //here we prevent double counting en-route passengers
                 printf("[FSM] Doors open at floor %d (en-route stop).\n", elevator->current_floor);
                 en_route_stop = false;
-            } else {
+            } else { //normal case, we count passenger served here and calc their wait time
                 int wait = current_tick - active_request.request_tick;
                 *total_wait += wait;
                 (*passengers_served)++;
                 printf("[FSM] Doors open at floor %d. Passenger wait time: %d ticks\n",
                        elevator->current_floor, wait);
             }
-            elevator->doors_open = true;
-            has_active_request = false;
-            elevator->current_state = STATE_IDLE;
+            elevator->doors_open = true; //this is just for simulation output to pring doors closing and opening, not really functional
+            has_active_request = false; //set has_active_request to false so that in next call we extract a new request
+            elevator->current_state = STATE_IDLE; //set state to idle for next call
             printf("[FSM] Transition -> IDLE\n");
         }
         break;
